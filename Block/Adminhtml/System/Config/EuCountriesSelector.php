53cb83f910119d4575e65470ccb1dacace105eff
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2019-01-24 17:52:00 +0100

    Implemented button to select only EU countries in Allow Countries

---
+++ Block/Adminhtml/System/Config/EuCountriesSelector.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MageSuite\EuCountriesSelector\Block\Adminhtml\System\Config;
+
+class EuCountriesSelector extends \Magento\Config\Block\System\Config\Form\Field
+{
+    protected $_template = 'MageSuite_EuCountriesSelector::system/config/eu_countries_selector.phtml';
+
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Config\Model\Config\Structure $configStructure
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Retrieve element HTML markup.
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     *
+     * @return string
+     */
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $this->setNamePrefix($element->getName())
+            ->setHtmlId($element->getHtmlId());
+
+        return $this->_toHtml();
+    }
+
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        return $this->_getElementHtml($element);
+    }
+}
